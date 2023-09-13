f65dfe25a34eb4421ded37fe5c34b4c3cd4d8f38
Hadrien Mens-Pellen <hadrien.mens-pellen@beta.gouv.fr>
2023-09-13 18:50:33 +0200

    :building_construction: Move to just

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
         devShell = pkgs.mkShell {
           packages = with pkgs; [
             hugo
+            just
             imageworsener
             nodePackages.gitmoji-cli
           ];
